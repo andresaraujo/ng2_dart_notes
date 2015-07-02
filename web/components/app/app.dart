bc97893165ad22c9bbff431aec2b41bd26defa82
Andres Araujo <a.araujo.azua@gmail.com>
2015-07-02 09:47:16 -0500

    refactor: change appInjector with viewInjector to reflect alpha 29 API changes

--- web/components/app/app.dart
+++ web/components/app/app.dart
@@ -4,7 +4,7 @@ import 'package:angular2/angular2.dart';
 import '../../services/note_store.dart' show Store, Note, NoteFactory;
 import '../card/card.dart';
 
-@Component(selector: 'app', appInjector: const [Store, NoteFactory])
+@Component(selector: 'app', viewInjector: const [Store, NoteFactory])
 @View(
     templateUrl: 'components/app/app.html',
     directives: const [NgFor, formDirectives, Card])
