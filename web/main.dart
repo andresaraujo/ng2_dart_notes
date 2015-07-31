73d29b170ae23dcb1c20cf66300aa6982596d706
Andres Araujo <a.araujo.azua@gmail.com>
2015-07-31 13:25:44 -0500

    chore: update to alpha 33

--- web/main.dart
+++ web/main.dart
@@ -1,7 +1,7 @@
 // Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-import 'package:angular2/angular2.dart';
+import 'package:angular2/bootstrap.dart';
 
 // These imports will go away soon:
 import 'package:angular2/src/reflection/reflection.dart' show reflector;
